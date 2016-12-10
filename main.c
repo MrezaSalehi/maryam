be99373319543c4d449acc391e6191729e7149b9
Amin Talebi <talebia.ce.sharif.edu>
2016-12-10 17:20:40 +0330

    fss

--- main.c
+++ main.c
@@ -1,7 +1,14 @@
 #include<stdio.h>
 
 int isPrime(int num) {
-
+	if(num == 1)
+		return 0;
+	int i = 0;
+	for(i = 2; i * i < n; i++){
+		if(num % i == 0)
+			return 0;
+		return 1;	
+	}
 }
 
 int absVal(int num) {
@@ -9,6 +16,6 @@ int absVal(int num) {
 }
 
 int main() {
-	printf("Hello world!");
+	printf("Hello Ahar!");
 	return 0;	
-}؛
+}
