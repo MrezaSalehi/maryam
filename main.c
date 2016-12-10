69198e5025ea1342b11b0c65e20b9f643d903498
Mrezasalehi <Mohammadreza.salehi95@sharif.edu>
2016-12-10 17:13:03 +0330

    file is changed

--- main.c
+++ main.c
@@ -2,13 +2,22 @@
 
 int isPrime(int num) {
 
+
+
+
 }
 
 int absVal(int num) {
+
+if (num>0)
+    return num;
+else
+    return (-num);
+
 
 }
 
 int main() {
 	printf("Hello world!");
-	return 0;	
+	return 0;
 }؛
